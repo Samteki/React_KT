510feccc474a230c6bc0c53da7eea40fcac70d44
Smart8878 <samrathpanchal8878@gmail.com>
2024-01-20 21:31:51 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { Hello, By } from './components/Hello';
+
+function App() {
+
+  return (
+    <div className="App">
+      <Hello />
+      <By />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Hello.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const Hello = ()=>{
+  return(
+  <div>
+    <h1>hello World</h1>
+  </div>)
+}
+
+export const By = ()=>{
+  return(
+  <div>
+    <h1>hello World</h1>
+  </div>)
+}
